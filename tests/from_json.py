63ff83b0c966b68cfa082f9c4b67fa3a78fa4c61
PioApocalypse <pioapocalypse@proton.me>
2025-02-17 16:08:17 +0100

    test for posting from json file with example json

---
+++ tests/from_json.py
@@ -0,0 +1,14 @@
+from amore.api.client import create_sample
+import json
+
+def test_create_sample():
+    with open('tests/exsample.json', 'r') as file:
+        data = json.load(file)
+    title = data['title']
+    body = data['body']
+    tags = data['tags']
+
+    create_sample(title=title, body=body, status="", tags=tags, substrate_batch="", position="")
+
+
+test_create_sample()
