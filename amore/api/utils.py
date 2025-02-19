7b6c61ba343eb957aef3e4ff068601bc4a5fe31f
PioApocalypse <pioapocalypse@proton.me>
2025-02-19 15:45:28 +0100

    id generator now works with placeholders and rng

--- amore/api/utils.py
+++ amore/api/utils.py
@@ -2,17 +2,24 @@ import os
 import requests
 from datetime import datetime
 from dotenv import load_dotenv
+from random import randrange as rr # TO REMOVE after feature is finished
+
+"""
+Utils for AMORE to read and elaborate internal ID's
+"""
 
 def get_std_id():
     # query to get all std_id
     # compare std_id, pick biggest
     # make sure std_id is number not string
     # make sure first 2 digits are less or equal to current year
-    return last_id # placeholder
-
+    last_id = 25000 + rr(1,999,1) # placeholder
+    return last_id
+    
 def get_location():
     # from user input get location
     # match known locations or return exception
+    location_name = "Napoli" # placeholder
     match location_name:
         case "Napoli":
             location_code = "Na"
@@ -27,12 +34,15 @@ def get_location():
         case _:
             print("Your location doesn't match any place in the code and will be treated as an exception.")
             location_code = "Xx"
-    return location_code # placeholder
+    return location_code
 
-def standard_id():
+def id_generator():
     last_id = get_std_id()
     last_year = last_id // 1000 # first 2 digits
     last_unique = last_id % 1000 # last 3 digits
+    if last_unique > 998:
+        print("Number of samples created this year exceeds 1000.\nSomething clearly isn't working as intended.\nPlease contact your sysadmin.")
+        return 1
 
     current_year = datetime.now().strftime("%y")
     if last_year < int(current_year):
@@ -43,4 +53,15 @@ def standard_id():
     std_id = int(current_year)*1000 + unique
     location_code = get_location()
     complete_id = f"{location_code}-{current_year}-{unique:03d}"
-    return std_id, complete_id
+    return std_id, complete_id, last_id # last_id only for testing purposes
+
+if __name__=="__main__":
+    print("Debug mode.")
+    id_array = id_generator()
+    std_id = id_array[0]
+    complete_id = id_array[1]
+    last_id = id_array[2]
+    print(f"Last ID: {last_id}, STD-ID: {std_id}, Name: {complete_id}")
+# TO REMOVE when this file is ready
+else: 
+    print("Feature not ready yet.")
