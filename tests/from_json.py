09aa5dddfa146d2e3537cd63db2a134884fe70e0
PioApocalypse <pioapocalypse@proton.me>
2025-02-19 15:46:25 +0100

    adds rng to tests to create unique ids every time

--- tests/from_json.py
+++ tests/from_json.py
@@ -1,14 +1,17 @@
 from amore.api.client import create_sample
 import json
+from random import randrange as rr
 
 def test_create_sample():
     with open('tests/exsample.json', 'r') as file:
         data = json.load(file)
-    title = data['title']
     body = data['body']
     tags = data['tags']
+    #std_id = data['std_id']
+    std_id = 25000 + rr(1,999,1)
+    title = f"Na-{std_id // 1000}-{std_id % 1000} -- {data['title']}"
 
-    create_sample(title=title, body=body, status="", tags=tags, substrate_batch="", position="")
+    create_sample(title=title, body=body, status="", tags=tags, std_id=std_id, substrate_batch="", position="")
 
 if __name__ == "__main__":
     test_create_sample()
--- tests/post_sample.py
+++ tests/post_sample.py
@@ -1,5 +1,12 @@
 from amore.api.client import create_sample
 from datetime import datetime
+from random import randrange as rr
+
+'''
+WARNING! Unique ID is randomly generated.
+This playground is meant to be nuked as soon
+as this project is put in production.
+'''
 
 def test_create_sample():
     mytitle = "Test sample"
@@ -7,10 +14,12 @@ def test_create_sample():
     status = 2
     tags = ["test", "primo campione"]
     body = "Bla bla bla"
+    unique = f"{rr(1,999,1)}" # unique is randomly generated
+    std_id = 25101
     substrate_batch = "Batch 1"
     position = "A1"
 
-    create_sample(title, body, status, tags, substrate_batch, position)
+    create_sample(title, body, status, tags, std_id, substrate_batch, position)
     #populate_sample(title, date, status, tags, body, substrate_batch, position)
     #assert "id" in response
     #assert response["title"] == title
