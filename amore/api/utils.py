f4b0b4cf11e246b0af5fdf327cc64ceb3eadb890
PioApocalypse <pioapocalypse@proton.me>
2025-02-20 13:20:47 +0100

    reworks method to get location codes

--- amore/api/utils.py
+++ amore/api/utils.py
@@ -2,42 +2,31 @@ import os
 import requests
 from datetime import datetime
 from dotenv import load_dotenv
+from amore.var import locations as ll
 from random import randrange as rr # TO REMOVE after feature is finished
 
 """
 Utils for AMORE to read and elaborate internal ID's
 """
 
-def get_std_id():
-    # query to get all std_id
+def get_location():
+    # city from user input
+    city = random.choice(['Salerno','Roma','Napoli']) # placeholder to try diff locations
+    #city = "Salerno" # placeholder to try multiple ID's
+    location_code = ll.location_to_code(city)
+    return location_code
+
+def get_std_id(loc_code):
+    # query to get all std_id FROM LOCATION
     # compare std_id, pick biggest
     # make sure std_id is number not string
     # make sure first 2 digits are less or equal to current year
-    last_id = 25000 + rr(1,999,1) # placeholder
+    last_id = 25000 + rr(1,999,1) # placeholder to try higher ID's
     return last_id
     
-def get_location():
-    # from user input get location
-    # match known locations or return exception
-    location_name = "Napoli" # placeholder
-    match location_name:
-        case "Napoli":
-            location_code = "Na"
-        case "Salerno":
-            location_code = "Sa"
-        case "Genova":
-            location_code = "Ge"
-        case "Roma":
-            location_code = "Rm"
-        case "L'Aquila":
-            location_code = "Aq"
-        case _:
-            print("Your location doesn't match any place in the code and will be treated as an exception.")
-            location_code = "Xx"
-    return location_code
-
 def id_generator():
-    last_id = get_std_id()
+    location_code = get_location()
+    last_id = get_std_id(location_code)
     last_year = last_id // 1000 # first 2 digits
     last_unique = last_id % 1000 # last 3 digits
     if last_unique > 998:
@@ -49,9 +38,7 @@ def id_generator():
         unique = 1
     else:
         unique = last_unique + 1
-
     std_id = int(current_year)*1000 + unique
-    location_code = get_location()
     complete_id = f"{location_code}-{current_year}-{unique:03d}"
     return std_id, complete_id, last_id # last_id only for testing purposes
 
