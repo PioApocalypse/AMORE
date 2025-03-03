77b7fce2faea2ee0eb7e0d60b843f8bafc2b56c5
PioApocalypse <pioapocalypse@proton.me>
2025-03-03 14:43:11 +0100

    create_sample now returns id of newly created sample

--- amore/api/client.py
+++ amore/api/client.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 from datetime import datetime
 from dotenv import load_dotenv
 
@@ -19,6 +20,10 @@ ED: about the key - would it be better to just implement a login page?
 The software would save the api key of each user locally in cache and
 discard it as soon as the user logs out...
 """
+
+# ========================
+# == EXPERIMENT SECTION ==
+# ========================
 
 def create_experiment(title, date, status, tags, b_goal, b_procedure, b_results):
     # a post request needs url, header and payload
@@ -48,42 +53,34 @@ def create_experiment(title, date, status, tags, b_goal, b_procedure, b_results)
     response.raise_for_status()
     return response.json()
 
-# =====================
+# =======================
+# == RESOURCES SECTION ==
+# =======================
 
-def create_sample(title, body, status, tags, std_id): #, substrate_batch, position):
-    items_url = f"{full_elab_url}""items/"
+def get_new_sample():
+    API_URL = os.getenv('ELABFTW_BASE_URL')
+    API_KEY = os.getenv('API_KEY')
+    every_id = []
     header = {
         "Authorization": API_KEY,
         "Content-Type": "application/json"
     }
-    payload = {
-        "template": 10, # 10 defines this item as 'sample' in our database
-        "title": title,
-        "body": body,
-        "status": status,
-        "tags": tags,
-        "metadata": '{ "extra_fields": { "STD-ID": { "type": "number", "value": "'+str(std_id)+'" } } }', # todo: add other fields
-        "custom_id": None,
-    }
+
+    search_query = f'{API_URL}api/v2/items?limit=9999'
     
-    try:
-        response = requests.post(
-            url=items_url,
-            headers=header,
-            json=payload,
-            verify=True
-        )
-        # figure out what's wrong with the following code which returns error:
-        #response.raise_for_status()
-        return 0 # response.json()
-    except:
-        print('An error occurred.')
-        return 1
+    response = requests.get(
+    headers=header,
+    url=search_query,
+    verify=True
+    )
+    
+    # returns 'id' from entry whose 'id' is max among all entries in response.json()
+    # thank god someone on stackoverflow had my same problem...
+    new_sample = max(response.json(), key=lambda ev: ev['id'])
+    return new_sample
 
-
-# =====================
 '''
-def populate_sample(title, date, status, tags, body, substrate_batch, position):
+def patch_sample(title, date, status, tags, body, substrate_batch, position):
     # like before, different url
     items_url = f"{full_elab_url}""items/0/"
     
@@ -93,29 +90,66 @@ def populate_sample(title, date, status, tags, body, substrate_batch, position):
     }
 
     payload = {
-        "title": title,
-        "date": date,
-        "status": status,
-        "tags": tags, # "tag 1|tag 2|tag 3"
         "body": body,
-        "category": 10, # 10 defines this item as 'sample' in our database
-        "metadata": {
-            "substrate_batch": substrate_batch,
-            "position": position
-        }
+        "metadata": '{ "extra_fields": { "STD-ID": { "type": "number", "value": "'+str(std_id)+'" } } }', # todo: add other fields
+        "custom_id": None
     }
 
-    response = requests.post(
+    try:
+        response = requests.post(
         url=items_url,
         headers=header,
         json=payload,
-        verify=False
-    )
-
-    response.raise_for_status()
-    return response.json()
+        verify=True
+        )
+        response.raise_for_status()
+        return 0
+    except:
+        print('An error occurred during item patching.')
+        return 1
 '''
+
+def create_sample(title, status, tags): #, substrate_batch, position):
+    items_url = f"{full_elab_url}""items/"
+    header = {
+        "Authorization": API_KEY,
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "template": 10, # 10 defines this item as 'sample' in our database
+        "title": title,
+        "status": status,
+        "tags": tags
+    }
+    
+    try:
+        # request item creation with template, title, status and tags as instructed by user
+        response = requests.post(
+            url=items_url,
+            headers=header,
+            json=payload,
+            verify=True
+        )
+        # get new sample metadata as dictionary:
+        new_sample = get_new_sample()
+        # get [elabftw] id of newly created sample:
+        new_elabid = new_sample['id']
+
+        # patch_sample(new_elabid)
+
+        # get std-id [yyxxx] and std-name [Aa-yy-xxx] of newly created sample:
+        new_stdid = json.loads(new_sample["metadata"])["extra_fields"]["STD-ID"]["value"]
+        new_stdname = new_sample["title"][:9]
+        print(f"{new_elabid},{new_stdid},{new_stdname}") # DEBUG
+        
+        return new_elabid, new_stdid, new_stdname
+    except:
+        print('An error occurred during item creation.')
+        return 1
+
+
 
 # disaster prevention
 if __name__ == "__main__":
     print('This file is not meant to be executed as main program.')
+    #return 1
