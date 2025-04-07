946059286ef442fa506810fc49903a1a48287ff5
PioApocalypse <pioapocalypse@proton.me>
2025-04-07 15:21:11 +0200

    tests/positions: adds class Position, misc. changes

--- tests/positions.py
+++ tests/positions.py
@@ -17,6 +17,51 @@ positions = [
     "Intro-2",
     "Intro-lost" ]
 
+class Position:
+    def __init__(self,
+                 title,
+                 id,
+                 avail=False):
+        self.title = title
+        self.id = id
+        self.avail = avail
+        self.data = [ i.strip() for i in title.replace("POS", "").split("-") ]
+    def getmachine(self):
+        return self.data[0]
+    def getslot(self):
+        slot = self.data[-1]
+        if slot.lower() == "lost":
+            return "LOST"
+        return slot
+    def getsector(self):
+        l = len(self.data)
+        if l < 3:
+            return None
+        elif l == 3:
+            return self.data[1]
+        else:
+            sectorname = ""
+            for i in self.data[1:l-1]:
+                sectorname += f" - {i}"
+            return sectorname[3:]
+    def isthisloss(self):
+        if self.data[-1].lower() == "lost":
+            # I  II
+            # II  L
+            return True
+        return False
+    def dump(self):
+        s = {
+            "title": self.title,
+            "id": self.id,
+            "avail": self.avail,
+            "machine": self.getmachine(),
+            "slot": self.getslot(),
+            "sector": self.getsector(),
+            "lost": self.isthisloss()
+            }
+        return s
+
 def parse_slots(dictlist):
     # 0. If somehow in prod scientists will still use longdash as separator:
     dictlist = [ { 'title': item.get('title').replace("–","-"), 'id': item.get('id') } for item in dictlist ]
@@ -26,16 +71,16 @@ def parse_slots(dictlist):
         split_title = item.get('title').split("-")
         # 2. Strip every element in list to remove opening/trailing spaces.
         stripped = [ i.strip() for i in split_title ]
-        # 3. Add every element in list into values of original dictionary with keys 'instrument', 'area', 'slot'.
-        if stripped[-1].lower() == "lost": # lost is treated as an area and not a slot
+        # 3. Add every element in list into values of original dictionary with keys 'instrument', 'sector', 'slot'.
+        if stripped[-1].lower() == "lost": # lost is treated as a sector and not a slot
             item["instrument"] = stripped[0]
-            item["area"] = "LOST"
+            item["sector"] = "LOST"
         elif len(stripped) == 2: # instrument and slot only, for smaller instruments
             item["instrument"] = stripped[0]
             item["slot"] = stripped[-1]
-        elif len(stripped) >= 3: # instrument, area and slot, for larger instruments (only really supports up to lenght 3)
+        elif len(stripped) >= 3: # instrument, sector and slot, for larger instruments (only really supports up to lenght 3)
             item["instrument"] = stripped[0]
-            item["area"] = stripped[-2]
+            item["sector"] = stripped[-2]
             item["slot"] = stripped[-1]
     return dictlist
 
@@ -44,12 +89,15 @@ def list_available_slots(dictlist):
     parsed_dictlist = parse_slots(dictlist)
     mylist = []
     for item in parsed_dictlist:
-        id = item.get('id')
-        print(id)
-        endpoint = f"{API_URL}api/v2/items/{id}/items_links/0/"
-        header = { "Authorization": API_KEY, "Content-Type": "application/json" }
-        response = requests.get( url=endpoint, headers=header, verify=True )
-        mylist.append( response.json() )
+        if item.get('sector') == "LOST":
+            mylist.append( item.get('title') )
+        else:
+            id = item.get('id')
+            endpoint = f"{API_URL}api/v2/items/{id}/items_links/" 
+            header = { "Authorization": API_KEY, "Content-Type": "application/json" }
+            response = requests.get( url=endpoint, headers=header, verify=True ).json() # returns 403 forbidden if I don't have write permission (github.com/elabftw/elabftw/issue/5577)
+            if isinstance(response, list) and len(response) == 0:
+                mylist.append( item.get('title') ) #{ 'title': item.get('title'), 'id': id} )
 
     return mylist
 
