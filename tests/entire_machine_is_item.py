c148640b9f9db7a20201105695e82be19adc686b
PioApocalypse <pioapocalypse@proton.me>
2025-04-10 17:31:55 +0200

    continuing tests with position tracking
    I have successfully created a functional class whose methods get every
    sector, every slot with their associated samples and sectors and much
    more

--- tests/entire_machine_is_item.py
+++ tests/entire_machine_is_item.py
@@ -6,9 +6,9 @@ from amore.api import client
 
 '''
 This approach assumes that every item from category "SAMPLE POSITIONS"
-on our eLabFTW is a different machine. This method keeps track of sample
-position by assigning items to a metadata field in eLabFTW whose key is
-the name/identifier of the slot. Example:
+on our eLabFTW represents a different machine. This method keeps track
+of sample position by assigning items to a metadata field in eLabFTW
+whose key is the name/identifier of the slot. Example:
 
     "extra_fields": {
         "main sector": {
@@ -49,104 +49,102 @@ class Position:
                  metadata):
         self.title = title
         self.id = id
-        self.meta = json.loads(metadata) # was this the right syntax ???
-        #self.data = [ item.strip() for item in self.title.replace("POS","",1).split("-") ]
-    def getsectors(self):
-        dic = self.meta.get("extra_fields") # all keys in extra_fields must be names of slots
-        return [ key for key in dic ]
+        self.meta = metadata # testing
+        #self.meta = json.loads(metadata) # in practice
+        self.extra = self.meta.get("extra_fields") or [""] # all keys in extra_fields must be names of slots
+        self.groups = self.meta.get("elabftw").get("extra_fields_groups") or [""] # list of dictionaries containing every sector's id and name
+    def getsectors(self): # returns list of sector ids and names
+        sectors = [ { "id": item.get("id"), "name": item.get("name") } for item in self.groups ]
+        return sectors
+        #dic = self.meta.get("extra_fields_groups")[0]
+        #return [ item.get('name') for item in dic ]
     def getslots(self):
-        dic = self.meta.get("extra_fields") # all keys in extra_fields must be names of slots
         slotlist = []
-        for key in dic: # for every sector in extra fields
-            for slot in dic.get(key): # for every slot in sector
-                slotname = f"{self.title}-{key}-{slot}"
-                slotsample = dic.get(key).get(slot)
-                response = {
-                    "slot": slotname,
-                    "sample": slotsample
-                }
-                slotlist.append(response) # object containing full location of the slot ("slot" key) and the sample associated ("sample" key)
+        for slot in self.extra: # for every slot in extra fields
+            slotname = f"{self.title} - {slot}"
+            slotsample = self.extra.get(slot).get("value")
+            slotgroup = self.extra.get(slot).get("group_id")
+            slotsector = [item.get("name")
+                for item in self.getsectors()
+                if item.get("id") == slotgroup][0]
+            response = {
+                "name": slotname,
+                "sector": slotsector,
+                "sample": slotsample
+            }
+            slotlist.append(response) # object containing full location of the slot ("slot" key) and the sample associated ("sample" key)
         return slotlist
-    """
-    Note for self: a method can call another method like this:
-
-        def mymethod(self):
-            # shit happens
-            return integer
-        def addone(self):
-            return self.method_1() + 1
-    """
     def getmachine(self):
         return self.title # if we opt for the "every machine is an item" approach
         #return self.data[0] # if we opt for the "every slot of every machine is an item" approach
-    def getsample(self, slot, sector="LOST"):
+    def getsample(self, slot):
         sector_slots = self.meta.get(sector)
         locate_sample = sector_slots.get(slot) or ""
-        if locate_sample #or locate_sample != "None":
+        if locate_sample: #or locate_sample != "None":
             return locate_sample
         return None
-    def isthisloss(self):
+    def isthisloss(self): # returns full titles
         if self.data[-1].lower() == "lost":
             # I  II
             # II  L
             return True
         return False
-    def dump(self):
-        s = {
-            "title": self.title,
-            "id": self.id,
-            "avail": self.avail,
-            "machine": self.getmachine(),
-            "slot": self.getslot(),
-            "sector": self.getsector(),
-            "lost": self.isthisloss()
-            }
-        return s
+    # def dump(self):
+    #     for item in self.getslots():
 
-def parse_slots(dictlist):
-    # 0. If somehow in prod scientists will still use longdash as separator:
-    dictlist = [ { 'title': item.get('title').replace("–","-"), 'id': item.get('id') } for item in dictlist ]
-    # Now iterate through elements of dictlist:
-    for item in dictlist:
-        # 1. Split element title by "-", creating list.
-        split_title = item.get('title').split("-")
-        # 2. Strip every element in list to remove opening/trailing spaces.
-        stripped = [ i.strip() for i in split_title ]
-        # 3. Add every element in list into values of original dictionary with keys 'instrument', 'sector', 'slot'.
-        if stripped[-1].lower() == "lost": # lost is treated as a sector and not a slot
-            item["instrument"] = stripped[0]
-            item["sector"] = "LOST"
-        elif len(stripped) == 2: # instrument and slot only, for smaller instruments
-            item["instrument"] = stripped[0]
-            item["slot"] = stripped[-1]
-        elif len(stripped) >= 3: # instrument, sector and slot, for larger instruments (only really supports up to lenght 3)
-            item["instrument"] = stripped[0]
-            item["sector"] = stripped[-2]
-            item["slot"] = stripped[-1]
-    return dictlist
+    #     s = {
+    #         "title": self.title,
+    #         "id": self.id,
+    #         "avail": self.avail,
+    #         "machine": self.getmachine(),
+    #         "slot": self.getslot(),
+    #         "sector": self.getsector(),
+    #         "lost": self.isthisloss()
+    #         }
+    #     return s
+
+# def list_available_slots(dictlist):
+#     parsed_dictlist = parse_slots(dictlist)
+#     mylist = []
+#     for item in parsed_dictlist:
+#         if item.get('sector') == "LOST":
+#             mylist.append( item.get('title') )
+#         else:
+#             id = item.get('id')
+#             endpoint = f"{API_URL}api/v2/items/{id}/items_links/" 
+#             header = { "Authorization": API_KEY, "Content-Type": "application/json" }
+#             response = requests.get( url=endpoint, headers=header, verify=True ).json() # returns 403 forbidden if I don't have write permission (github.com/elabftw/elabftw/issue/5577)
+#             if isinstance(response, list) and len(response) == 0:
+#                 mylist.append( item.get('title') ) #{ 'title': item.get('title'), 'id': id} )
+
+#     return mylist
 
 
-def list_available_slots(dictlist):
-    parsed_dictlist = parse_slots(dictlist)
-    mylist = []
-    for item in parsed_dictlist:
-        if item.get('sector') == "LOST":
-            mylist.append( item.get('title') )
-        else:
-            id = item.get('id')
-            endpoint = f"{API_URL}api/v2/items/{id}/items_links/" 
-            header = { "Authorization": API_KEY, "Content-Type": "application/json" }
-            response = requests.get( url=endpoint, headers=header, verify=True ).json() # returns 403 forbidden if I don't have write permission (github.com/elabftw/elabftw/issue/5577)
-            if isinstance(response, list) and len(response) == 0:
-                mylist.append( item.get('title') ) #{ 'title': item.get('title'), 'id': id} )
+# with open('config.json') as cfg:
+#     config = json.load(cfg)
 
-    return mylist
-
-
-with open('config.json') as cfg:
-    config = json.load(cfg)
-
-API_KEY = config.get('API_KEY')
-API_URL = config.get('ELABFTW_BASE_URL')
+# API_KEY = config.get('API_KEY')
+# API_URL = config.get('ELABFTW_BASE_URL')
 #actualpos = client.get_positions(API_KEY)
 #print(list_available_slots(actualpos))
+
+with open('tests/machines.json') as f:
+    machines = json.load(f)
+
+machinesObj = []
+for machine in machines:
+    machineObj = Position(machine.get('title'), machine.get('id'), machine.get('metadata')) # machineObj is class 'position'
+    machinesObj.append(machineObj)
+
+# My list of slots is now created, let's put it to good use: checking if a slot is clear or not.
+available = []
+not_available = []
+for machineObj in machinesObj:
+    slots = machineObj.getslots() # list of objects with 'name', 'sector' and 'sample' keys
+    for slot in slots:
+        if slot.get('sample'):
+            not_available.append(slot)
+        else:
+            available.append(slot)
+print(available)
+print(not_available)
