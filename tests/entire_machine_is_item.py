9f3a165bc4e472b5e35beb1b0d6b5286fcaddc5e
PioApocalypse <pioapocalypse@proton.me>
2025-04-11 15:27:57 +0200

    tests/every..: Instrument class and methods now fully documented
    docstrings have been added to the class every one of its methods
    also, "machine" is being replaced by "instrument"
    
    ↑ next commit will be a purge ↑

--- tests/entire_machine_is_item.py
+++ tests/entire_machine_is_item.py
@@ -6,7 +6,7 @@ from amore.api import client
 
 '''
 This approach assumes that every item from category "SAMPLE POSITIONS"
-on our eLabFTW represents a different machine. This method keeps track
+on our eLabFTW represents a different instrument. This method keeps track
 of sample position by assigning items to a metadata field in eLabFTW
 whose key is the name/identifier of the slot. Example:
 
@@ -43,22 +43,38 @@ positions = [
     "Intro-lost" ]
 
 class Instrument:
+    '''Class created to handle different instruments within eLabFTW.'''
     def __init__(self,
                  title,
                  id,
                  metadata):
+        '''
+        Init method. Allows self.title, self.id, self.meta,
+        self.extra, self.groups.
+
+        meta is the content of the metadata field (dict);
+        extra and groups both get objects from self.meta:
+        extra gets the extra_fields dict, groups gets the
+        extra_fields_groups dict.
+        '''
         self.title = title
         self.id = id
         self.meta = metadata # testing
         #self.meta = json.loads(metadata) # in practice
         self.extra = self.meta.get("extra_fields") or [""] # all keys in extra_fields must be names of slots
         self.groups = self.meta.get("elabftw").get("extra_fields_groups") or [""] # list of dictionaries containing every sector's id and name
-    def getsectors(self): # returns list of sector ids and names
+    def getsectors(self):
+        '''
+        Returns list of dictionaries with every sector's id and name.
+        '''
         sectors = [ { "id": item.get("id"), "name": item.get("name") } for item in self.groups ]
         return sectors
-        #dic = self.meta.get("extra_fields_groups")[0]
-        #return [ item.get('name') for item in dic ]
     def getslots(self):
+        '''
+        Returns list of dictionaries with full location of
+        the slot ("name"), name of its sector ("sector"),
+        and the sample associated to it ("sample").
+        '''
         slotlist = []
         for slot in self.extra: # for every slot in extra fields
             slotname = f"{self.title} - {slot}"
@@ -74,17 +90,14 @@ class Instrument:
             }
             slotlist.append(response) # object containing full location of the slot ("name" key), sector name ("sector" key) and the sample associated ("sample" key)
         return slotlist
-    def getmachine(self): # equivalent to self.title
-        return self.title # if we opt for the "every machine is an item" approach
-        #return self.data[0] # if we opt for the "every slot of every machine is an item" approach
-    def getavailable(self):
-        slotlist = self.getslots()
-        available = [ { "name": f"{self.title} - LOST", "sector": "LOST", "sample": "" } ]
-        for slot in slotlist:
-            if slot.get("sample") == "":
-                available.append(slot)
-        return available
-    def isthisloss(self): # returns only lost samples
+    def getinstrument(self):
+        '''Equivalent to self.title.'''
+        return self.title
+    def isthisloss(self):
+        '''
+        Returns list of slots (see "getslots()") whose
+        "sector" is equal to "LOST" (case insensitive).
+        '''
         # I  II
         # II  L
         lost = []
@@ -92,12 +105,50 @@ class Instrument:
             if slot.get("sector").lower() == "lost":
                 lost.append(slot)
         return lost
-    def free_lost(self): # returns number (int) of smallest available lost slot in the machine, eg. if LOST - 1 through LOST - 5 are assigned it returns 6
+    def freelost(self): 
+        '''
+        Returns number (integer) of the smallest available
+        slot in the instrument's LOST sector.
+
+        E.g.: if LOST - 1 through LOST - 5 are assigned it
+        returns 6 - even if slot LOST - 3 is unassigned.
+
+        Takes as input "isthisloss()" method's output.
+        '''
         lost = self.isthisloss()
         latest_lost = max(lost, key=lambda x: int(x.get("name").split("-")[-1]))
         latest_lost_number = int(latest_lost.get("name").split("-")[-1])
         return latest_lost_number +1
+    def getavailable(self):
+        '''
+        Returns list of slots (see "getslots()") whose
+        "sample" value is empty (""); it always includes
+        the smallest available LOST sector slot (see
+        "freelost()").
+        '''
+        slotlist = self.getslots()
+        available = [ { "name": f"{self.title} - LOST - {self.freelost()}", "sector": "LOST", "sample": "" } ]
+        for slot in slotlist:
+            if slot.get("sample") == "":
+                available.append(slot)
+        return available
     # def dump(self):
+        '''
+        Returns list of dictionaries which is a full dump
+        of every single existent slot of every single
+        instrument available as a resource on eLabFTW.
+
+        Every element of the list contains:
+            - "instrument_id": Instrument.itemID
+            - "instrument_title": Instrument.title
+            - "sector_name": Sector.name
+            - "slot_name": Slot.name
+            - "sample_id": Sample.itemID (if present)
+            - "sample_title": Sample.title (if present)
+
+        Remember:   items have titles and ids,
+                    metadata have names.
+        '''
     #     for item in self.getslots():
 
     #     s = {
@@ -157,9 +208,11 @@ for machineObj in machinesObj:
     #         not_available.append(slot)
     #     else:
     #         available.append(slot)
-    available_test.append(machineObj.getavailable())
-    lost.append(machineObj.isthisloss())
-    freelost.append(machineObj.free_lost())
+    for item in machineObj.isthisloss():
+        lost.append(item)
+    for item in machineObj.getavailable():
+        available_test.append(item)
+    freelost.append(machineObj.freelost())
 print(lost)
 # print(available)
 print(available_test)
